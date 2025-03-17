4d6d08434ec762be4afd29541576b82919e24f29
HaHa <haidanglink0@gmail.com>
2025-03-17 16:26:28 +0700

    update

--- src/components/HPreview/HPreview.jsx
+++ src/components/HPreview/HPreview.jsx
@@ -14,22 +14,13 @@ const HPreview = ({
     footerID = null,
  }) => {
 
-      const [childContent, setChildContent] = useState(<></>);
     const handleisPrinted = () => {
         isPrinted(true)
     }
 
-    // useEffect(() => {
-    //     if (children) {
-    //         setChildContent(<div>{children}</div>)
-    //     }
-
-    // }, [children,isPrint]);
-
-
 
     const pageCss = generatePageNumberCss(pageNumberStyle ? pageNumberStyle : { style: {}, format: 'Trang {page}/{pages}', isBottomPosition: false })
-    const newCssVarriable = createCssVarriable(layoutSetup ? layoutSetup : { width: '210mm', height: '297mm', headerHeight: '50mm', footerHeight: '20mm', marginLeft: "15mm", marginRight: '20mm', paddingTop: '15mm', paddingBottom: '15mm' })
+    const newCssVarriable = createCssVarriable(layoutSetup ? layoutSetup : { width: '210mm', height: '297mm', headerHeight: '30mm', footerHeight: '20mm', marginLeft: "15mm", marginRight: '20mm', paddingTop: '15mm', paddingBottom: '15mm' })
 
     return (
         <CreatePreviewWithPrint
